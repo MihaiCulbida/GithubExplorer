78c61436a8639820195ca8332ed1073ea5514c77
MihaiCulbida <clbidamihai@gmail.com>
2026-02-04 11:23:45 +0200

    Show data

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ btn.addEventListener('click', () => {
     fetch(`https://api.github.com/users/${username}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data); 
+            document.getElementById('result').innerHTML = `
+                <img src="${data.avatar_url}" width="100">
+                <h2>${data.name}</h2>
+                <p>Followers: ${data.followers}</p>
+                <p>Repos: ${data.public_repos}</p>
+                <p>Bio: ${data.bio}</p>
+            `;
         });
 });
