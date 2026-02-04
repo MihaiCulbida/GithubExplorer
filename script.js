565a0a3f8eed53c85560a603e7019a4b91f8c066
MihaiCulbida <clbidamihai@gmail.com>
2026-02-04 11:23:45 +0200

    Show all  data

--- script.js
+++ script.js
@@ -9,9 +9,17 @@ btn.addEventListener('click', () => {
             document.getElementById('result').innerHTML = `
                 <img src="${data.avatar_url}" width="100">
                 <h2>${data.name}</h2>
+                <p>@${data.login}</p>
+                <p>Bio: ${data.bio || 'N/A'}</p>
+                <p>Location: ${data.location || 'N/A'}</p>
+                <p>Website: ${data.blog || 'N/A'}</p>
+                <p>Twitter: ${data.twitter_username || 'N/A'}</p>
+                <p>Company: ${data.company || 'N/A'}</p>
                 <p>Followers: ${data.followers}</p>
-                <p>Repos: ${data.public_repos}</p>
-                <p>Bio: ${data.bio}</p>
+                <p>Following: ${data.following}</p>
+                <p>Public Repos: ${data.public_repos}</p>
+                <p>Member since: ${new Date(data.created_at).toLocaleDateString()}</p>
+                <img src="https://github-readme-stats.vercel.app/api?username=${data.login}&show_icons=true">
             `;
         });
 });
